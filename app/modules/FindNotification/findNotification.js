a371c537359171b33fd44338a245026fe025fc48
unknown <biprono12@gmail.com>
2019-03-15 16:26:59 +0700

    Chia modules

---
+++ app/modules/FindNotification/findNotification.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from 'react-native'
+import { Navigation } from 'react-native-navigation'
+
+export default class FindNotification extends React.Component {
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
---
+++ app/modules/UploadDocument/uploadDocument.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from 'react-native'
+import { Navigation } from 'react-native-navigation'
+
+export default class UploadDocument extends React.Component {
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
